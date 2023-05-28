f66e05feed291035ee8b8e7bf3bdc1afa507e2b0
Prasenjeet999 <jeetprasenjeet999@gmail.com>
2023-05-27 22:37:01 -0600

    Initial commit

---
+++ R/Square_fun.R
@@ -0,0 +1,14 @@
+#' Trial Square Function
+#'
+#' @param x A numeric value
+#'
+#' @return A numeric value of the squared of number
+#' @export
+#'
+#' @examples
+#' x <- 4
+#' square_fun(4)
+square_fun <- function(x){
+  y = x^2
+  return (y)
+}
